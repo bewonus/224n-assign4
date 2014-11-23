faf5acc31c92e594c9cf1578aef7d43816ad575d
Brandon Taylor Ewonus <bewonus@corn18.stanford.edu>
2014-11-23 09:53:29 -0800

    added random initialization of W and U matrices

--- pa4/java/src/cs224n/deep/WindowModel.java
+++ pa4/java/src/cs224n/deep/WindowModel.java
@@ -1,4 +1,5 @@
 package cs224n.deep;
+
 import java.lang.*;
 import java.util.*;
 
@@ -10,9 +11,9 @@ import java.text.*;
 
 public class WindowModel {
 
-	protected SimpleMatrix L, W, Wout, U, b1, b2; // word-vector matrix, weight matrix, weight matrix out
+  protected SimpleMatrix L, W, Wout, U, b1, b2; // word-vector matrix, weight matrix, weight matrix out
 
-	public int windowSize, wordSize, hiddenSize; // C, n, H
+  public int windowSize, wordSize, hiddenSize; // C, n, H
   public double learningRate; // alpha
   public int numLabels = 5;
 
@@ -26,28 +27,41 @@ public class WindowModel {
    * @param _hiddenSize = H
    * @param _lr = the learning rate (alpha)
    */
-	public WindowModel(int _windowSize, int _hiddenSize, double _lr) {
-		windowSize = _windowSize;
+  public WindowModel(int _windowSize, int _hiddenSize, double _lr) {
+    windowSize = _windowSize;
     hiddenSize = _hiddenSize;
     learningRate = _lr;
-	}
+  }
 
-	/**
-	 * Initializes the weights randomly. 
-	 */
-	public void initWeights(){
-
-    // TODO: randomly initialize these guys later...
-
+  /**
+   * Initializes the weights randomly.
+   */
+  public void initWeights() {
     wordSize = FeatureFactory.allVecs.numRows(); // == 50
 
     // word-vector matrix (i.e. the x's)
     L = new SimpleMatrix(FeatureFactory.allVecs);
 
     // initialize with bias inside as the last column (plus 1)
-    W = new SimpleMatrix(hiddenSize, windowSize * wordSize + 1);
-    U = new SimpleMatrix(numLabels, hiddenSize + 1);
-	}
+//    W = new SimpleMatrix(hiddenSize, windowSize * wordSize + 1);
+//    U = new SimpleMatrix(numLabels, hiddenSize + 1);
+
+    // set params for random initialization
+    double eW = Math.sqrt(6.0) / Math.sqrt(hiddenSize + windowSize * wordSize);
+    double eU = Math.sqrt(6.0) / Math.sqrt(numLabels + hiddenSize);
+
+    // randomly initialize W and U
+    W = SimpleMatrix.random(hiddenSize, windowSize * wordSize + 1, -eW, eW, new java.util.Random());
+    U = SimpleMatrix.random(numLabels, hiddenSize + 1, -eU, eU, new java.util.Random());
+
+    // initialize bias terms to 0
+    for (int i = 0; i < W.numRows(); i++) {
+      W.set(i, W.numCols() - 1, 0);
+    }
+    for (int i = 0; i < U.numRows(); i++) {
+      U.set(i, U.numCols() - 1, 0);
+    }
+  }
 
   /**
    * Create the concatenated vector x
@@ -58,30 +72,37 @@ public class WindowModel {
     SimpleMatrix x = new SimpleMatrix(wordSize * windowSize + 1, 1);
     int counter = 0;
     for (int col : indices) {
-      System.out.println(indices);
       for (int row = 0; row < wordSize; row++) {
         x.set(counter * wordSize + row, 0, L.get(row, col));
       }
       counter++;
     }
+    // add an intercept term at the end of x
     x.set(counter * wordSize, 0, 1);
     return x;
   }
 
 
+  /**
+   * Create a window of word indices centered at index
+   * @param _trainData list of training data
+   * @param index center index of window
+   * @return list of indices
+   */
   private List<Integer> createWindow(List<Datum> _trainData, int index) {
 
     List<Integer> indices = new ArrayList<Integer>(windowSize);
     Boolean startSeen = false;
     Boolean endSeen = false;
 
-    for (int i = index; i >= index - (windowSize - 1)/2; i--) {
-      if (startSeen || _trainData.get(i).word.equals(START)) {
+    // start of the window
+    for (int i = index; i >= index - (windowSize - 1) / 2; i--) {
+      if (startSeen || _trainData.get(i).word.equals(START)) { // handle start tokens
         startSeen = true;
         indices.add(0, FeatureFactory.wordToNum.get(START));
-      }else {
+      } else {
         Integer nextIndex = FeatureFactory.wordToNum.get(_trainData.get(i).word.toLowerCase());
-        if (nextIndex == null) {
+        if (nextIndex == null) { // handle words not contained in the vocabulary
           indices.add(0, FeatureFactory.wordToNum.get(UNK));
         } else {
           indices.add(0, nextIndex);
@@ -89,13 +110,14 @@ public class WindowModel {
       }
     }
 
-    for (int i = index+1; i <= index + (windowSize - 1)/2; i++) {
-      if (endSeen || _trainData.get(i).word.equals(END)) {
+    // end of the window
+    for (int i = index + 1; i <= index + (windowSize - 1) / 2; i++) {
+      if (endSeen || _trainData.get(i).word.equals(END)) { // handle end tokens
         endSeen = true;
         indices.add(FeatureFactory.wordToNum.get(END));
-      }else {
+      } else {
         Integer nextIndex = FeatureFactory.wordToNum.get(_trainData.get(i).word.toLowerCase());
-        if (nextIndex == null) {
+        if (nextIndex == null) { // handle words not contained in the vocabulary
           indices.add(FeatureFactory.wordToNum.get(UNK));
         } else {
           indices.add(nextIndex);
@@ -106,57 +128,77 @@ public class WindowModel {
     return indices;
   }
 
+  /**
+   * Apply the function f (hyperbolic tangent) to every element of x
+   * @param x
+   * @return
+   */
   private SimpleMatrix fTransform(SimpleMatrix x) {
-    SimpleMatrix toReturn = new SimpleMatrix(x.numRows() +1 , x.numCols());
-    for(int i = 0 ; i < x.numRows() ; i++){
-      double val = Math.tanh(x.get(i,0));
+    SimpleMatrix toReturn = new SimpleMatrix(x.numRows() + 1, x.numCols());
+    for (int i = 0; i < x.numRows(); i++) {
+      double val = Math.tanh(x.get(i, 0));
       toReturn.set(i, 0, val);
     }
-    toReturn.set(toReturn.numRows()-1,0,1);
+    // add an intercept entry at the end of x
+    toReturn.set(toReturn.numRows() - 1, 0, 1);
     return toReturn;
   }
 
+  /**
+   * Apply the function g (softmax) to every element of x
+   * @param x
+   * @return
+   */
   private SimpleMatrix gTransform(SimpleMatrix x) {
     double norm = 0;
-
-    for(int i = 0 ; i < x.numRows() ; i++){
-      double exp = Math.exp(x.get(i,0));
+    for (int i = 0; i < x.numRows(); i++) {
+      double exp = Math.exp(x.get(i, 0));
       norm += exp;
-      x.set(i,0,exp);
+      x.set(i, 0, exp);
     }
+    // normalize x
     x = x.divide(norm);
     return x;
   }
 
-  private SimpleMatrix makeWout(SimpleMatrix x) {
-      SimpleMatrix afterF = fTransform(W.mult(x));
-      return gTransform(U.mult(afterF));
+
+  /**
+   * Apply the feed-forward function to the input window x
+   * @param x
+   * @return
+   */
+  private SimpleMatrix feedForward(SimpleMatrix x) {
+    SimpleMatrix afterF = fTransform(W.mult(x));
+    return gTransform(U.mult(afterF));
   }
 
-	/**
-	 * Simplest SGD training
-	 */
-	public void train(List<Datum> _trainData ){
+  /**
+   * Simplest SGD training
+   */
+  public void train(List<Datum> _trainData) {
 
     for (int i = 0; i < _trainData.size(); i++) {
 
+      // ignore sentence start and end tokens
       if (_trainData.get(i).word.equals(START) || _trainData.get(i).word.equals(END)) {
         continue;
       }
 
+      // create input matrix x
       List<Integer> windowIndices = createWindow(_trainData, i);
-
       SimpleMatrix x = makeX(windowIndices);
 
-      Wout = makeWout(x);
+      // apply feed-forward network function
+      Wout = feedForward(x);
+
       System.out.println(Wout);
     }
 
-	}
+  }
 
-	
-	public void test(List<Datum> testData){
-		// TODO
-		}
-	
+
+  public void test(List<Datum> testData) {
+    // TODO
+  }
+
 }
